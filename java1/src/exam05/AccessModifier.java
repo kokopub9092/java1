ba62bbd295ddc42418c40e7b330976f6abf6a11e
DJA <DJA@DJA-PC>
2017-07-23 11:22:50 +0900

    170723

--- java1/src/exam05/AccessModifier.java
+++ java1/src/exam05/AccessModifier.java
@@ -5,11 +5,6 @@ import exam04.Modifier;
 public class AccessModifier extends Modifier{
 	
 	public static void main(String[] args) {
-		AccessModifier e = new AccessModifier();
-		e.pubNum = 3;
-//		e.priNum = 4;
-		e.defNum = 5;
-		e.proNum = 6;
 		
 	}
 
